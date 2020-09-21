c5e0276c69a3b842c1fb84e71902483bdddbd215
Jay <jay1999ke@gmail.com>
2020-09-21 11:44:50 +0530

    Recaptcha validation

--- splits/views.py
+++ splits/views.py
@@ -8,6 +8,9 @@ from .models import account, person, friend, f_list, make_group, expense
 from django.template import loader
 import decimal
 from django.conf import settings
+import urllib
+import json
+from django.contrib import messages
 
 # Create your views here.
 from django.http import HttpResponse
@@ -154,7 +157,23 @@ class login(View):
         return render(request, 'splits/mainpage.html',context)
 
     def post(self,request):
+        ''' Begin reCAPTCHA validation '''
+        recaptcha_response = request.POST.get('g-recaptcha-response')
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        values = {
+            'secret': settings.SERVER_KEY,
+            'response': recaptcha_response
+        }
+        data = urllib.parse.urlencode(values).encode()
+        req =  urllib.request.Request(url, data=data)
+        response = urllib.request.urlopen(req)
+        result = json.loads(response.read().decode())
+        ''' End reCAPTCHA validation '''
 
+        if result['success']:
+            messages.success(request, 'New comment added with success!')
+        else:
+            return HttpResponse("Captcha Failed... Try again")
         uname= request.POST.get('uname','')
         password = request.POST.get('psw','')
         user = authenticate(username=uname, password=password)
